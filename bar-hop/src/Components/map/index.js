b4ff1e115e5375463c569010c360ea29b43aef1f
Zach Hollingsworth <zahollingsworth@Zachs-MacBook.local>
2019-06-29 13:44:08 -0400

    map stuff

---
+++ bar-hop/src/Components/map/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SimpleMap from "./map";
+import "./style.css";
+
+
+function Maps() {
+    return (
+        <div className="global">
+            <SimpleMap/>
+        </div>
+    );
+  }
+  
+  export default Maps;
