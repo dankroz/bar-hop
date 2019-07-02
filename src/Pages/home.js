cd1193d0958947b0393cca293ebbb15ba787d87e
Harold Bernard <haroldbernard@Mayas-MacBook-Air.local>
2019-07-01 21:55:40 -0400

    added pages and tried button component

---
+++ src/Pages/home.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Jumbotron from "../Components/Jumbotron";
+import Button from "../Components/Button";
+
+
+class Home extends Component {
+
+    render() {
+        return (
+            
+        <div>
+            <Jumbotron />
+            <Button />
+        </div>
+        )
+    }
+
+}
+
+export default Home;
