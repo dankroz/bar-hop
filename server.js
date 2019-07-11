bf6f206a2594373cda44007ec1c9aebac8cd17f0
Dan Kroszner <dankroz@gmail.com>
2019-07-11 16:37:12 -0400

    add .env.development and updated seedDB.js

--- server.js
+++ server.js
@@ -22,3 +22,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/barhop");
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
+
+
+
+
+
+
