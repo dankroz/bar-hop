4ba4c52132459adb675eb93a2957b802ec3a7e60
Dan Kroszner <dankroz@gmail.com>
2019-07-13 13:54:20 -0400

    adding in passport things

--- config/passport.js
+++ config/passport.js
@@ -9,16 +9,18 @@ passport.deserializeUser(function (user, done) {
 passport.use(
     new GoogleStrategy(
         {
-            clientID: "GOOGLE_CLIENT_ID",
-            clientSecret: "GOOGLE_CLIENT_SECRET",
+            clientID: "997739765485-j16uv3tptbnls4cq4u9e6n3era5c28e1.apps.googleusercontent.com",
+            clientSecret: "UjMxCUxiYSeuhxL7OzPmOMIU",
             callbackURL: "http://localhost:3000/auth/google/callback"
         },
         function (accessToken, refreshToken, profile, done) {
             var userData = {
                 email: profile.emails[0].value,
                 name: profile.displayName,
+                highscore: profile.highscore,
                 token: accessToken
             };
+            console.log(userData);
             done(null, userData);
         }
     )
