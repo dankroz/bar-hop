49ebd594634514011d672455d78fbef251ac34c0
Zach Hollingsworth <zahollingsworth@Zachs-MacBook.local>
2019-07-13 10:59:16 -0400

    home page updates

--- client/src/Pages/home.js
+++ client/src/Pages/home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "../Components/Background/style.css";
 import Button from "../Components/Button"
+import API from "../Utils/API"
 
 const background = {
     backgroundColor: "#0A2463",
@@ -12,11 +13,35 @@ const background = {
 }
 
 class Home extends Component {
+    state = {
+        bars: [],   
+      };
+      
+      componentDidMount() {
+        this.getLocation();
+        this.loadBars();
+      }
+    
+    
+    getLocation = () => { 
+    
+        navigator.geolocation.getCurrentPosition(function(position) {
+            console.log(position.coords.latitude, position.coords.longitude);
+          });
+    }
+    
+    loadBars = () => {
+        API.getBars()
+          .then(res => this.setState({ bars: res.data }))
+          .catch(err => console.log(err));
+        console.log(this.state.bars);
+        
+      };
 
     render() {
         return (
         <div style={background}>
-            <Button onClick={this.clicked}>
+            <Button onClick={this.loadBars}>
             Start Bar Hop
             </Button>
          </div>
