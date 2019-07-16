3fde5214f02a948d6f71126aab5b27e42e1bef37
Zach Hollingsworth <zahollingsworth@Zachs-MacBook.local>
2019-07-15 21:51:13 -0400

    Long Time Coming

--- client/src/Pages/home.js
+++ client/src/Pages/home.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import "../Components/Background/style.css";
 import Button from "../Components/Button"
-import API from "../Utils/API"
+// import API from "../Utils/API"
+//import ShopContext from "../context/shop-context"
 
 const background = {
     backgroundColor: "#0A2463",
@@ -13,40 +14,131 @@ const background = {
 }
 
 class Home extends Component {
-    state = {
-        bars: [],   
-      };
-      
-      componentDidMount() {
-        this.getLocation();
-        this.loadBars();
-      }
-    
-    
-    getLocation = () => { 
-    
-        navigator.geolocation.getCurrentPosition(function(position) {
-            console.log(position.coords.latitude, position.coords.longitude);
-          });
-    }
-    
-    loadBars = () => {
-        API.getBars()
-          .then(res => this.setState({ bars: res.data }))
-          .catch(err => console.log(err));
-        console.log(this.state.bars);
+    // state = {
+    //     bars: [],
+    //     Userlong: "",
+    //     Userlat: "",
+    //     closest: "",
         
-      };
+    //   };
+      
+
+  
+    // componentDidMount() {
+  
+    //     this.loadBars();
+    //     this.getLocation();
+        
+    //   };
+    
+    
+    // getLocation = () => { 
+    //     navigator.geolocation.getCurrentPosition(function(position) {
+    //         console.log("hello")
+    //         console.log(position.coords.latitude, position.coords.longitude);   
+    //       })
+    // };
+    
+//     loadBars = () => {
+//         API.getBars()
+//           .then(res => this.setState({ bars: res.data }))
+//           .catch(err => console.log(err));
+//         console.log(this.state.bars);
+//         // console.log(this.getLocation())
+//         //this.Loading();
+//       };
+
+//     loadBars1 = () => {
+//         API.getBars()
+//           .then(res => this.setState({ bars: res.data }))
+//           .catch(err => console.log(err));
+//         console.log(this.state.bars);
+//         // console.log(this.getLocation())
+//         this.Loading();
+//         //return  this.props.history.push("/map")
+//       };
 
 
-//     clicked = () => {
-//         return this.props.history.push("/map");
-//     }
+//     Loading = () => {
+//         // HTML5/W3C Geolocation
+//         if (navigator.geolocation) {
+//           navigator.geolocation.getCurrentPosition(this.UserLocation);     
+//         }      
+//         // Default to Washington, DC
+//         else
+//           console.log("No Geolocation")
+//       }
+
+//     UserLocation = (position)  => {
+//         this.NearestCity(position.coords.latitude, position.coords.longitude);
+        
+//         this.setState({Userlat: position.coords.latitude });
+//         this.setState({Userlong: position.coords.longitude })
+      
+//         console.log("Lat:" + this.state.Userlat + ", Long:" + this.state.Userlong);
+//       }
+    
+
+//     NearestCity = (latitude, longitude) => {
+        
+//         var minDif = 99999;
+//         var closest;
+        
+//         for (var index = 0; index < this.state.bars.length; ++index) {
+//           var dif = this.PythagorasEquirectangular(latitude, longitude, this.state.bars[index].latitude, this.state.bars[index].longtitude);
+//             console.log("Facts: " + this.state.bars[index].latitude, this.state.bars[index].longtitude)
+//           if (dif < minDif) {
+//             closest = index;
+//             minDif = dif;
+//           }
+//         }
+//         console.log("asdaf: " + closest);
+//         this.setState({closest: closest });
+//         console.log(this.state.bars[closest]);
+//         //this.setState({closestBar:this.state.bars[closest]});
+//         console.log("The Closest bar to our location is " + this.state.closestBar.name);
+//         //this.clicked()
+// }
+
+//     PythagorasEquirectangular = (lat1, lon1, lat2, lon2) => {
+//         lat1 = this.Deg2Rad(lat1);
+//         lat2 = this.Deg2Rad(lat2);
+//         lon1 = this.Deg2Rad(lon1);
+//         lon2 = this.Deg2Rad(lon2);
+//         var R = 6371; // km
+//         var x = (lon2 - lon1) * Math.cos((lat1 + lat2) / 2);
+//         var y = (lat2 - lat1);
+//         var d = Math.sqrt(x * x + y * y) * R;
+//         return d;
+//       }
+
+//       Deg2Rad = (deg) => {
+//         return deg * Math.PI / 180;
+//       }
+
+//       sendData = () => {
+//         this.props.parentCallback("Hey Popsie, How’s it going?");
+//    };
+
+     
+
+
+
+ 
+
+
+    clicked = () => {
+       return this.props.history.push("/map");
+    }
 
     render() {
+
         return (
         <div style={background}>
-            <Button onClick={this.loadBars}>
+            <p>{this.props.closestBar.name}</p>
+            <Button onClick={this.clicked}
+                    // onClick={this.context.addProductToCart(this.state.closestBar)}
+            >
             Start Bar Hop
             </Button>
          </div>
--- client/src/Pages/maps.js
+++ client/src/Pages/maps.js
@@ -4,22 +4,137 @@ import "../Components/Map/style.css";
 import GreenBanner from "../Components/Banner/index.js";
 import Button from "../Components/Button"
 import SmallBtn from "../Components/Button/SmallBtn"
+//import Home from "../Components/Desktop";
+//import API from "../Utils/API"
+
 
 class Maps extends Component {
+//     state = {
+//         bars: [],
+//         Userlong: "",
+//         Userlat: "",
+//         closest: "",
+//         closestBar: "",
+//       };
+      
+
+  
+//     componentDidMount() {
+//         this.getLocation();
+       
+       
+        
+        
+//       };
+    
+    
+//     getLocation = () => { 
+//         navigator.geolocation.getCurrentPosition(function(position) {
+//             console.log("hello")
+//             console.log(position.coords.latitude, position.coords.longitude);
+              
+//           })
+//           this.loadBars(); 
+//     };
+    
+//     loadBars = () => {
+//         API.getBars()
+//             // .then(res => console.log(res.data))
+//           .then(res => this.setState({ bars: res.data }))
+//           .catch(err => console.log(err));
+          
+//         console.log(this.state.bars);
+//         // console.log(this.getLocation())
+//         this.loadBars1();
+//       };
+
+//     loadBars1 = () => {
+//         API.getBars()
+//           .then(res => this.setState({ bars: res.data }))
+//           .catch(err => console.log(err));
+//         console.log(this.state.bars);
+//         // console.log(this.getLocation())
+//         this.Loading();
+//         // return  this.props.history.push("/map")
+//       };
+
+
+//     Loading = () => {
+//         // HTML5/W3C Geolocation
+//         if (navigator.geolocation) {
+//           navigator.geolocation.getCurrentPosition(this.UserLocation);     
+//         }      
+//         // Default to Washington, DC
+//         else
+//           console.log("No Geolocation")
+//       }
+
+//     UserLocation = (position)  => {
+//         this.NearestCity(position.coords.latitude, position.coords.longitude);
+        
+//         this.setState({Userlat: position.coords.latitude });
+//         this.setState({Userlong: position.coords.longitude })
+      
+//         console.log("Lat:" + this.state.Userlat + ", Long:" + this.state.Userlong);
+//       }
+    
+
+//     NearestCity = (latitude, longitude) => {
+        
+//         var minDif = 99999;
+//         var closest;
+        
+//         for (var index = 0; index < this.state.bars.length; ++index) {
+//           var dif = this.PythagorasEquirectangular(latitude, longitude, this.state.bars[index].latitude, this.state.bars[index].longtitude);
+//             console.log("Facts: " + this.state.bars[index].latitude, this.state.bars[index].longtitude)
+//           if (dif < minDif) {
+//             closest = index;
+//             minDif = dif;
+//           }
+//         }
+//         console.log("asdaf: " + closest);
+//         this.setState({closest: closest });
+//         console.log(this.state.bars[closest]);
+//         this.setState({closestBar:this.state.bars[closest]});
+//         //console.log("The Closest bar to our location is " + this.state.closestBar.name);
+//         //this.clicked()
+// }
+
+//     PythagorasEquirectangular = (lat1, lon1, lat2, lon2) => {
+//         lat1 = this.Deg2Rad(lat1);
+//         lat2 = this.Deg2Rad(lat2);
+//         lon1 = this.Deg2Rad(lon1);
+//         lon2 = this.Deg2Rad(lon2);
+//         var R = 6371; // km
+//         var x = (lon2 - lon1) * Math.cos((lat1 + lat2) / 2);
+//         var y = (lat2 - lat1);
+//         var d = Math.sqrt(x * x + y * y) * R;
+//         return d;
+//       }
+
+//       Deg2Rad = (deg) => {
+//         return deg * Math.PI / 180;
+//       }
+
+
+
+
     clicked = event => {
         event.preventDefault();
         console.log("hello");
     }
     render() {
+        
         return (
             <div className="global">
                 <SmallBtn>
                     ?
                 </SmallBtn>
-                <GreenBanner>HINT GOES HERE </GreenBanner>
+                <GreenBanner > {this.props.closestBar.hint1} </GreenBanner>
                 <SimpleMap />
-                <Button onClick={this.clicked}>
-                    Check Progress
+              
+                <Button onClick={this.loadBars}>
+                    Start Game
                 </Button>
             </div>
         );
