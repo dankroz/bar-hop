4c2f87cd4374fed32a932e8cb628e69e98603502
Dan Kroszner <dankroz@gmail.com>
2019-07-18 13:52:48 -0400

    took out console.log on didupdate

--- client/src/Pages/Leaderboard.js
+++ client/src/Pages/Leaderboard.js
@@ -39,7 +39,6 @@ class Leaderboard extends Component {
 
   getUserScore = () => {
     const id = this.props.auth.user.id
-    console.log(id)
     API.getUser(id)
       .then(res => this.setState({userscore: (res.data.highscore + 100)}))
   }
