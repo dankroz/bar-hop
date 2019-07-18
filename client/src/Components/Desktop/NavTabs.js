8489ac6bdc1776a9b8f01c66c7b9a5d17b964c70
Alex <afb5309@psu.edu>
2019-07-18 14:32:44 -0400

    new phone images on desktop

--- client/src/Components/Desktop/NavTabs.js
+++ client/src/Components/Desktop/NavTabs.js
@@ -11,11 +11,11 @@ function NavTabs(props) {
          <Navbar.Collapse id="responsive-navbar-nav">
              <Nav className="ml-auto">
                  <NavDropdown title="More Info" id="collasible-nav-dropdown">
-                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                     <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                     <NavDropdown.Item href="https://github.com/HaroldBernard/bar-hop" target="_blank">Github</NavDropdown.Item>
+                     {/* <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                      <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                      <NavDropdown.Divider />
-                     <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                     <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
                  </NavDropdown>
 
                 <Nav.Link 
